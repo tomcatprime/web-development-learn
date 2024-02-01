0f54bbf6a50255b7f2a380d77ca7703986a4ed04
tomcatprime <karolchru@gmail.com>
2024-02-02 00:45:50 +0200

    functions

--- functions/funtions.js
+++ functions/funtions.js
@@ -76,5 +76,9 @@ function sumArray(number){
     return sum;
 }
 
-//Write a function called returnDay. this function takes in one parameter (a number from 1-7) and returns the day of the week (1 is Monday, 2 is Tuesday, etc.)  If the number is less than 1 or greater than 7, the function should return null. In some countries Sunday is treated as the first day of the week, but for this exercise we will use Monday as the first day.
-
+//returning functions
+function makeBetweenFunc(min, max) {
+    return function(num){
+        return num >= min && num <= max;
+    }
+}
