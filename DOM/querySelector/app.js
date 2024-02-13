8d3d85bd2716178f76a25b9dd8592727c1f64871
tomcatprime <karolchru@gmail.com>
2024-02-13 18:48:12 +0200

    query selector

---
+++ DOM/querySelector/app.js
@@ -0,0 +1,49 @@
+document.querySelector('#banner')
+
+document.querySelector('.square')  //by class
+
+
+//example of selecting with css selector 
+const checkbox = document.querySelector("input[type='checkbox']")
+
+
+//manipulating
+
+const allLinks = document.querySelectorAll('a');
+
+for (let link of allLinks){
+    link.innerText = 'I am a link !!!!'
+}
+
+//manipulating text example
+document.querySelector('span').innerHTML = "<span>Good</span>"
+
+
+///manipulate DOM
+/*
+Let's get some practice working with DOM element attributes. I've provided you with some very basic markup.  Please select the image element and:
+
+change its source to this url: https://devsprouthosting.com/images/chicken.jpg
+
+change its alt text to be "chicken"
+*/
+const eggImg = document.querySelector('img');
+
+eggImg.src = 'https://devsprouthosting.com/images/chicken.jpg';
+eggImg.alt = 'chicken';
+
+
+/*
+use JavaScript to make the following changes:
+
+Select the div with the id of container.  Using JavaScript, set it's text alignment to 'center';
+
+Select the image and use JavaScript to give it a width of 150px and a border radius of 50%
+
+Remember to use camelCased property names in JavaScript! (backgroundColor, not background-color, etc.)*/
+const container = document.querySelector('#container');
+container.style.textAlign = 'center';
+
+const image = document.querySelector("img");
+image.style.width = '150px';
+image.style.borderRadius = '50%';
---
+++ DOM/selecting/app.js
@@ -0,0 +1,21 @@
+//selecting by id - document.getElementById("id")
+
+//selecting by tag - document.getElementByTagName('tag') - selectings all with spicified tag
+
+/*
+const allImages = document.getElementsByTagName('img');
+
+for( let img of allImages){
+    console.log(img.src)
+}
+
+*/
+
+//selecting by class
+
+/*
+const square = document.getElementByClassName('square')
+
+for(let img of square){
+    im.src = 'www.google.com/testimg.img'
+}
