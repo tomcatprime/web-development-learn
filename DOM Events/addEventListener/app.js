c866c29b43a524b40e52e14ccaf4e31929b4e814
tomcatprime <karolchru@gmail.com>
2024-02-14 15:28:45 +0200

    change Color simple JS

--- DOM Events/addEventListener/app.js
+++ DOM Events/addEventListener/app.js
@@ -7,3 +7,14 @@ const btn3 = document.querySelector('#v3');
 btn.addEventListener('click', function (){
     alert("CLICKED")
 }
+
+
+const button = document.querySelector('#hello')
+button.addEventListener('click', function(){
+    console.log('hello')
+})
+
+const byebutton = document.querySelector('#goodbye')
+byebutton.addEventListener('click', function(){
+    console.log('goodbye')
+})
