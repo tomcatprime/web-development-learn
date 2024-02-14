072e772c30b723d45efb99b9f1d001b058e7c143
tomcatprime <karolchru@gmail.com>
2024-02-14 13:32:55 +0200

    DOM events

---
+++ DOM Events/addEventListener/app.js
@@ -0,0 +1,9 @@
+document.querySelector('h1').onclick = () => {
+    alert('you clicked the h1')
+}
+
+
+const btn3 = document.querySelector('#v3');
+btn.addEventListener('click', function (){
+    alert("CLICKED")
+}
